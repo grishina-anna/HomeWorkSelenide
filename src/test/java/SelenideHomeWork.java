eaac7463ad421a7b412cb656cde5606a037666e7
grish <grishina@factor-ts.ru>
2022-01-01 21:46:37 +0300

    01.01.2022

---
+++ src/test/java/SelenideHomeWork.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.textCaseSensitive;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideHomeWork {
+
+    @BeforeEach
+        void BeforeEach() {
+        clearBrowserCookies();
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void shouldFindSoftAssertionsInWiki(){
+        open("https://github.com/"); //открыть страницу
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter(); //в поиске ввести selenide
+        $$("ul.repo-list li").first().$("a").click(); //заходим в первый результат
+        $("h1").shouldHave(textCaseSensitive("selenide / selenide")); //в заголовке h1 должен быть текст selenide / selenide
+        $x("//*[@data-content='Wiki']").click(); //перейти на Wiki
+        $(".Layout-sidebar").$(withText("more page")).click(); //Раскрыть все вкладки, нажав more page
+        $(".Layout-sidebar").$(byText("SoftAssertions")).click(); //дойти до SoftAssertions, зайти
+        $(byText("Using JUnit5 extend test class:")).parent().sibling(0) //проверить, что встречается тескт Using JUnit5 extend test class:
+                .shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+    }
+}
